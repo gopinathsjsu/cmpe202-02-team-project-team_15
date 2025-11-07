a58b8256ca593d932cb331784b2c73621c208d15
abhishek-jha-24 <abhi2004jha@gmail.com>
2025-11-06 22:00:26 -0800

    removed my ports/endpoints

--- server/app.ts
+++ server/app.ts
@@ -37,13 +37,11 @@ export const app = express();
 // Middleware
 app.use(cors({
   origin: [
-    process.env.CLIENT_URL || 'http://localhost:3000',
+    process.env.CLIENT_URL ||
     'http://127.0.0.1:3000',
     'http://localhost:3000',
     'http://localhost:3002',
-    'http://127.0.0.1:3002',
-    'http://localhost:3001',
-    'http://127.0.0.1:3001'
+    'http://127.0.0.1:3002'
   ],
   credentials: true
 }));
