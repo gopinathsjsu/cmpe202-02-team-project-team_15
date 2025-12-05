198b6a2a621ed75763f0aa7d488c3cbd35bddf9d
kmanoj03 <manoj03.work@gmail.com>
2025-12-04 17:07:46 -0800

    fix heart overrun onto navbar issue

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -65,7 +65,7 @@ const Navbar: React.FC = () => {
   };
 
   return (
-    <header className="bg-white border-b border-gray-200 sticky top-0 z-10">
+    <header className="bg-white border-b border-gray-200 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
